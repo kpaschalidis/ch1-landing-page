50d9d5bcf32ea7f3bd52a0ac43dc743920b85f6e
Konstantinos Paschalidis <konstantinospaschalidis@validitylabs.org>
2020-06-07 15:06:43 +0200

    Fix subscribe form input.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import styles from './index.module.css';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div  style={{display: 'flex', justifyContent: 'center'}}>
-    <h1>Unfortunately you are too late 😔.</h1>
-    </div>
-   
-    <div  style={{display: 'flex', justifyContent: 'center'}}>
-    <h3>Game Over.</h3>
-    </div>
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div> */}
-    {/* <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
-    <div  style={{marginTop: '80px', display: 'flex', justifyContent: 'center'}}>
+class IndexPage extends React.Component {
+  state = {
+    email: ''
+  }
 
-    <h3>Don't miss the next one...</h3>
-    </div>
-    <div className={styles.formWrapper}> 
-    <form className={styles.form}>
-      <div>
-      <input className={styles.input} type="text" value="email" placeholder="Subscribe..."/>
-      </div>
-      <div>
-      <button className={styles.button}>
-        Subsribe Now
+  handleOnChange = (e) => {
+    this.setState({
+      email: e.currentTarget.value
+    })
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <h1>Unfortunately you are too late <span>😔</span>.</h1>
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <h3>Game Over.</h3>
+        </div>
+        <div style={{ marginTop: '80px', display: 'flex', justifyContent: 'center' }}>
+
+          <h3>Don't miss the next one...</h3>
+        </div>
+        <div className={styles.formWrapper}>
+          <form className={styles.form}>
+            <div>
+              <input onChange={this.handleOnChange} className={styles.input} type="text" value={this.state.email} placeholder="Subscribe..." />
+            </div>
+            <div>
+              <button className={styles.button}>
+                Subsribe Now
       </button>
-      </div>
-    </form>
-    </div>
-  </Layout>
-)
-
+            </div>
+          </form>
+        </div>
+      </Layout>
+    );
+  }
+}
 export default IndexPage
